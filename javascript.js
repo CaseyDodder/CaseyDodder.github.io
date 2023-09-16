98fd9da115dafa59eff0fcef8c6a34aca2fc8d1e
CaseyDodder <dodderc@outlook.com>
2023-09-15 23:47:34 -0500

    Added dropdowns, twitter video, other fixes.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   let currentSliderClass = '.default-slider';
+  // Racing Game Object
   const racingGameObject = {
     gameTitle: 'Racing Game',
     heroImage: 'images/racing-game/woods-track.png',
@@ -20,13 +21,32 @@ $(document).ready(function () {
       '16 Unique Tracks',
       'Local Co-op circuit mode',
     ],
-    aboutGame: "Get ready to embark on a nostalgic journey back to the golden era of arcade gaming with RetroRush! This top-down racing game pays homage to the classic arcade racers of yesteryears, offering an adrenaline-pumping experience that will have you hooked from the moment you hit the accelerator.</br></br>Local Co-op: Grab your friends and family for some retro-inspired racing fun. With local co-op support, you can race head-to-head or team up in Gauntlet Mode. Experience the joy of multiplayer arcade action on the same screen, just like the good old days.",
+    aboutGame: "Get ready to embark on a nostalgic journey back to the golden era of arcade gaming with RetroRush! This top-down racing game pays homage to the classic arcade racers of yesteryears, offering an adrenaline-pumping experience that will have you hooked from the moment you hit the accelerator.</br></br>",
     gameModes: [
-      "<span>Time Trial Mode:</span> Race against the clock as you navigate a series of challenging tracks. Precision and speed are the keys to victory in this mode, where shaving off milliseconds is the ultimate goal.",
-      "<span>Circuit Mode:</span> Immerse yourself in the world of championship racing as you compete in a thrilling series of circuits. Take on AI opponents in a fierce battle for first place. With 16 unique tracks and 16 reverse tracks, every race presents a new challenge, requiring mastery of both offensive and defensive driving skills.",
-      "<span>Gauntlet Mode:</span> Test your mettle in this high-stakes mode, where you'll face off against a relentless horde of AI racers. The pressure is on as you attempt to survive wave after wave of opponents while collecting power-ups to stay ahead.",
-    ]
+      [
+        "Local Co-op",
+        "Grab your friends and family for some retro-inspired racing fun. With local co-op support, you can race head-to-head or team up in Gauntlet Mode. Experience the joy of multiplayer arcade action on the same screen, just like the good old days.",
+        `<i class="fas fa-users"></i>`,
+      ],
+      [
+        "Time Trial Mode",
+        "Race against the clock as you navigate a series of challenging tracks. Precision and speed are the keys to victory in this mode, where shaving off milliseconds is the ultimate goal.",
+        `<i class="fas fa-stopwatch"></i>`,
+      ],
+      [
+        "Circuit Mode",
+        "Immerse yourself in the world of championship racing as you compete in a thrilling series of circuits. Take on AI opponents in a fierce battle for first place. With 16 unique tracks and 16 reverse tracks, every race presents a new challenge, requiring mastery of both offensive and defensive driving skills.",
+        `<i class="fas fa-flag-checkered"></i>`,
+      ],
+      [
+        "Gauntlet Mode",
+        "Test your mettle in this high-stakes mode, where you'll face off against a relentless horde of AI racers. The pressure is on as you attempt to survive wave after wave of opponents while collecting power-ups to stay ahead.",
+        `<i class="fas fa-car-crash"></i>`,
+      ],
+    ],
+    videoClass: '.racing-game-video',
   };
+  // Disc Golf Game Object
   const discGolfGameObject = {
     gameTitle: 'Disc Golf Arcade',
     heroImage: 'images/disc-golf-arcade/main-image.png',
@@ -48,6 +68,7 @@ $(document).ready(function () {
     ],
     aboutGame: "With intuitive controls and 100+ challenging levels, you'll be able to master your virtual disc throwing skills. Immerse yourself with unique 2D physics and dynamic environments, including forests, farms, beaches, space and the matrix! With each level offering a unique challenge, you'll be able to test your skills and improve your technique. Download now and join the fun!",
     gameModes: [],
+    videoClass: '#youtubePlayer',
   };
 
   $('.game-menu-link').on('click', function() {
@@ -105,8 +126,26 @@ $(document).ready(function () {
     // About Game Section.
     $('.about-game__container').empty();
     gameObject.gameModes.forEach(function (value) {
-      $('.about-game__container').append(`<div>${value}</div>`);
+      $('.about-game__container').append(`
+        <div class="game-mode-dropdown">
+          <div class="game-mode-dropdown--innner">
+            ${value[2]}
+            <span class="game-mode-title">${value[0]}</span>
+            <i class="fas fa-chevron-down"></i>
+          </div>
+          <div class="game-mode-dropdown-copy hidden">${value[1]}</div>
+        </div>`
+      );
     });
+
+    // VIDEO
+    $('.video-container div').each(function () {
+      $('#youtubePlayer').addClass('hidden');
+      $('.racing-game-video').addClass('hidden');
+      $(gameObject.videoClass).removeClass('hidden');
+      // $(this).hide();
+      // $('#youtubePlayer').addClass('hidden');
+    })
   }
 
   // SLICK SLIDER
@@ -152,4 +191,20 @@ $(document).ready(function () {
     });
   }
   initializeSlickSlider('.default-slider');
+
+  $('body').on('click', '.game-mode-dropdown', function () {
+    let arrowIcon = $(this).find('.fa-chevron-down');
+    let dropdownCopy = $(this).find(".game-mode-dropdown-copy");
+    if (!dropdownCopy.hasClass('hidden')) {
+      dropdownCopy.addClass('hidden');
+      $('.game-mode-dropdown .fa-chevron-down').css('transform', 'initial');
+    }
+    else {
+      $('.game-mode-dropdown .fa-chevron-down').css('transform', 'initial');
+      $('.game-mode-dropdown-copy').addClass('hidden');
+      // element.css('transform', 'rotate(180deg)');
+      $(this).find(".game-mode-dropdown-copy").removeClass('hidden');
+      arrowIcon.css('transform', 'rotate(180deg)');
+    }
+  });
 });
